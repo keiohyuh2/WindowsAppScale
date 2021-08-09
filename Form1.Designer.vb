67dbea82baa18fd4ffdc51949aac61f8cedb8fb9
keiohyuh <k.saito@keiohyuh.co.jp>
2021-08-09 17:43:19 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextScaleCurrent = New System.Windows.Forms.TextBox()
+        Me.TimerScaleScan = New System.Windows.Forms.Timer(Me.components)
+        Me.ButtonTimer = New System.Windows.Forms.Button()
+        Me.ButtonEnd = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextScaleConfirm = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MS UI Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label1.Location = New System.Drawing.Point(58, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(126, 19)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "現在の総重量"
+        '
+        'TextScaleCurrent
+        '
+        Me.TextScaleCurrent.BackColor = System.Drawing.SystemColors.Info
+        Me.TextScaleCurrent.Enabled = False
+        Me.TextScaleCurrent.Font = New System.Drawing.Font("MS UI Gothic", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.TextScaleCurrent.ForeColor = System.Drawing.SystemColors.InfoText
+        Me.TextScaleCurrent.Location = New System.Drawing.Point(62, 70)
+        Me.TextScaleCurrent.Name = "TextScaleCurrent"
+        Me.TextScaleCurrent.Size = New System.Drawing.Size(150, 28)
+        Me.TextScaleCurrent.TabIndex = 1
+        '
+        'TimerScaleScan
+        '
+        '
+        'ButtonTimer
+        '
+        Me.ButtonTimer.Font = New System.Drawing.Font("MS UI Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.ButtonTimer.Location = New System.Drawing.Point(321, 57)
+        Me.ButtonTimer.Name = "ButtonTimer"
+        Me.ButtonTimer.Size = New System.Drawing.Size(200, 50)
+        Me.ButtonTimer.TabIndex = 2
+        Me.ButtonTimer.Text = "計量モニター開始"
+        Me.ButtonTimer.UseVisualStyleBackColor = True
+        '
+        'ButtonEnd
+        '
+        Me.ButtonEnd.Font = New System.Drawing.Font("MS UI Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.ButtonEnd.Location = New System.Drawing.Point(321, 321)
+        Me.ButtonEnd.Name = "ButtonEnd"
+        Me.ButtonEnd.Size = New System.Drawing.Size(200, 50)
+        Me.ButtonEnd.TabIndex = 3
+        Me.ButtonEnd.Text = "終了"
+        Me.ButtonEnd.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("MS UI Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Button1.Location = New System.Drawing.Point(321, 149)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(200, 50)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "確定"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextScaleConfirm
+        '
+        Me.TextScaleConfirm.BackColor = System.Drawing.SystemColors.Info
+        Me.TextScaleConfirm.Enabled = False
+        Me.TextScaleConfirm.Font = New System.Drawing.Font("MS UI Gothic", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.TextScaleConfirm.ForeColor = System.Drawing.Color.Red
+        Me.TextScaleConfirm.Location = New System.Drawing.Point(62, 162)
+        Me.TextScaleConfirm.Name = "TextScaleConfirm"
+        Me.TextScaleConfirm.Size = New System.Drawing.Size(150, 28)
+        Me.TextScaleConfirm.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("MS UI Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label2.Location = New System.Drawing.Point(58, 140)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(109, 19)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "確定総重量"
+        '
+        'form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(684, 461)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextScaleConfirm)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ButtonEnd)
+        Me.Controls.Add(Me.ButtonTimer)
+        Me.Controls.Add(Me.TextScaleCurrent)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "form1"
+        Me.Text = "計量モニター画面"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextScaleCurrent As TextBox
+    Friend WithEvents TimerScaleScan As Timer
+    Friend WithEvents ButtonTimer As Button
+    Friend WithEvents ButtonEnd As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextScaleConfirm As TextBox
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,453 @@
+﻿Imports System.Text
+Imports System.Runtime.InteropServices
+Imports Newtonsoft.Json
+Imports System.Net
+Imports System.IO.Ports
+
+
+Public Class form1
+    ' iniファイルの場所を定数として宣言
+    Public Const INI_FILE_PATH As String = "./Scale.ini" '初期設定ファイル
+    '浜田様サブドメイン
+    Public Const SUB_DOMAIN = "kkhamada"  '参考：https://kkhamada.cybozu.com/k/485/
+
+    'kintone関連
+    'アプリID、kintone-APIのurl、トークンを、環境に合わせて設定
+    '■１．kintoneアプリのapp_id
+    'Public Const APP_ID = "3"  '問合せアプリ
+    'テスト環境の計量機重量アプリID
+    'Public Const APP_ID = "12"  '計量機重量アプリアプリ
+    '浜田様環境の計量機重量アプリID
+    Public Const APP_ID = "485"  '計量機重量アプリアプリ
+    '■２．url
+    ' kintoneアプリのURL 「複数」レコードGET
+    'Public Const KINTONE_GET_URL As String = "https://devotigoz.cybozu.com/k/v1/records.json" 'テスト環境 URL
+    Public Const KINTONE_GET_URL As String = "https://" & SUB_DOMAIN & ".cybozu.com/k/v1/records.json" 'kintone URL
+    ' kintoneアプリのURL レコード更新
+    Public Const KINTONE_UPD_URL As String = "https://" & SUB_DOMAIN & ".cybozu.com/k/v1/record.json" 'kintone URL
+    ' 問合せアプリのトークン
+    'Private Const SCALE_TOKEN As String = "v879Wvs6BjG5PDlg8z3QRXSOd1Y1B2Hao68RloSc" 'kintone 問合せアプリ
+    'テスト環境の計量機重量アプリのトークン
+    'Private Const SCALE_TOKEN As String = "UuehIDeFi4t4uyuhvJcHTz5GO2j4ZmtVkSxCwltu"
+    '浜田様環境の計量機重量アプリのトークン
+    Private Const SCALE_TOKEN As String = "XhXGDfliEepwOpqMJi8s5tfAzvGl2GIcH2vANgQt"    'kintone計量機重量アプリ
+
+
+    'クエリーの生成
+    Public Const KINTONE_APP As String = "?app=" & APP_ID 'kintone 問合せアプリ
+    ' kintoneアプリのquery(remarks="00001")
+    Public Const KINTONE_QUERY As String = "&query=" 'クエリTOP
+    'kintone query
+    Private query_str As String = "品目=""総重量"""   'ユニークキー：ユニーク値の組み合わせとする
+    'Private query_str As String = "remarks=""00005"""
+    Private query_enc As String = System.Net.WebUtility.UrlEncode(query_str)
+    Private query_kintone As String = KINTONE_QUERY & query_enc
+    'urlencodeが必要な時(日本語名等)System.Net.WebUtility.UrlEncodeを使用する
+    'サンプル：Dim urlenc_post_data = System.Net.WebUtility.UrlEncode(post_data)
+
+    ' 問合せアプリのGET URL
+    Private url_get As String = KINTONE_GET_URL & KINTONE_APP & query_kintone
+    ' 問合せアプリのPOST URL
+    Private post_get As String = KINTONE_UPD_URL
+
+    'ボタン表示名
+    Private Const TIMERSTART As String = "計量モニター開始" '計量モニタースタート/ストップボタン表示名
+    Private Const TIMERSTOP As String = "計量モニター中止" '計量モニタースタート/ストップボタン表示名
+
+    WithEvents mSerialPort As SerialPort = Nothing     ' 通信用シリアルポート変数
+    Private mPortName As String                     ' COM PORT
+    Private mBaudRate As Integer = Nothing　　　　　' 通信速度(機器側)
+    Private mDataLength As Integer = Nothing      ' データ長
+    Private mStopBits As Integer                  ' ストップビット 
+    Private mParity As Parity = Nothing           ' パリティ
+    Private mDataHeader As String = "ST,GS,"      ' 安定データを示す先頭データ
+
+    Private mRecvData As String                   ' 受信データ
+    Private mRDataLen As Integer = 10             ' 重量(+****.**)8桁 + 単位(kg)2桁
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'INIファイルクラスの生成
+        Dim Ini As New ClsIni(INI_FILE_PATH)
+
+        'INIファイルからのScaleとの通信設定情報取得
+        Dim str As String
+        str = Ini.GetProfileString("ScaleOCX", "Port", "2")
+        Console.WriteLine("ScaleOCX:Port=" & str)
+        mPortName = "COM" & str
+        str = Ini.GetProfileString("ScaleOCX", "Bps", "9600")
+        Console.WriteLine("ScaleOCX:Bps=" & str)
+        mBaudRate = Int(str)
+        str = Ini.GetProfileString("ScaleOCX", "DataLen", "7")
+        Console.WriteLine("ScaleOCX:DataLen=" & str)
+        mDataLength = Int(str)
+        str = Ini.GetProfileString("ScaleOCX", "Parity", "E")
+        Console.WriteLine("ScaleOCX:Parity=" & str)
+        If str = "E" Then
+            mParity = Parity.Even
+        Else
+            mParity = Parity.Odd
+        End If
+        str = Ini.GetProfileString("ScaleOCX", "StopLen", "1")
+        Console.WriteLine("ScaleOCX:StopLen=" & str)
+        mStopBits = Int(str)
+
+        'str = Ini.GetProfileString("ScaleOCX", "Handshake", "1")
+        'Console.WriteLine("ScaleOCX:Handshake=" & str)
+
+        str = Ini.GetProfileString("ScaleOCX", "DataHeader", "ST,GS,")
+        Console.WriteLine("ScaleOCX:DataHeader=" & str)
+        mDataHeader = str
+
+        'Scaleとの通信確認
+        'ここにコーディング
+        Try
+            mSerialPort = New SerialPort
+            mSerialPort.PortName = mPortName
+            mSerialPort.BaudRate = mBaudRate
+            mSerialPort.DataBits = mDataLength
+            mSerialPort.Parity = mParity
+            mSerialPort.StopBits = mStopBits
+            mSerialPort.Open()
+            Console.WriteLine("comOpen:success")
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            '終了
+            Application.Exit()
+        End Try
+
+        'タイマーボタン初期設定
+        ButtonTimer.Text = TIMERSTART
+        TimerScaleScan.Interval = 1000
+        TimerScaleScan.Enabled = True
+        TimerScaleScan.Stop()
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles TimerScaleScan.Tick
+        Me.RcvDataToTextBox(mRecvData)
+        'debug
+        'Me.TextScaleCurrent.Text = DateTime.Now.ToLongTimeString()
+    End Sub
+
+    Private Sub ButtonTimer_Click(sender As Object, e As EventArgs) Handles ButtonTimer.Click
+        If ButtonTimer.Text = TIMERSTART Then
+            ButtonTimer.Text = TIMERSTOP
+            TimerScaleScan.Start()
+        Else
+            ButtonTimer.Text = TIMERSTART
+            TimerScaleScan.Stop()
+        End If
+    End Sub
+
+    '終了処理
+    Private Sub ButtonEnd_Click(sender As Object, e As EventArgs) Handles ButtonEnd.Click
+        '通信クローズ
+        '処理記述
+        mSerialPort.Close()
+        '終了
+        Application.Exit()
+
+    End Sub
+
+    '****************************************************************************'
+    '*
+    '*	@brief	データ受信が発生したときのイベント処理.
+    '*
+    '*	@param	[in]	sender	イベントの送信元のオブジェクト.
+    '*	@param	[in]	e		イベント情報.
+    '*
+    '*	@retval	なし.
+    '*
+    Private Sub mSerialPort_DataReceived(ByVal sender As System.Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles mSerialPort.DataReceived
+
+        '受信データ格納変数
+        Dim mCurrentRecvData As String = mSerialPort.ReadLine()
+
+        'ヘッダ判定
+        If (mCurrentRecvData.StartsWith(mDataHeader)) Then
+            mRecvData = mCurrentRecvData.Substring(mDataHeader.Length, mRDataLen)
+            Console.WriteLine("安定受信データ：" & mRecvData)
+        Else
+            mRecvData = ""
+            Console.WriteLine("安定受信データ：なし")
+        End If
+
+    End Sub
+
+    '****************************************************************************'
+    '*
+    '*	@brief	受信データをテキストボックスに書き込む.
+    '*
+    '*	@param	[in]	data	受信した文字列.
+    '*
+    '*	@retval	なし.
+    '*
+    Private Sub RcvDataToTextBox(data As String)
+
+        '受信データをテキストボックスに表示する.
+        If IsNothing(data) = False Then
+            TextScaleCurrent.Text = data
+        End If
+
+    End Sub
+
+    '確定ボタン：総重量の現在値を取込、kintoneの値を更新する
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim id As String = "0"   '初期値
+        Dim debug_text As String = ""
+
+        '確定値を表示
+        TextScaleConfirm.ForeColor = Color.Red
+        TextScaleConfirm.Text = TextScaleCurrent.Text
+
+        Using client As New System.Net.Http.HttpClient()
+            'ヘッダ編集
+            'client.DefaultRequestHeaders.Accept.Clear()
+            client.DefaultRequestHeaders.Add("X-Cybozu-API-Token", SCALE_TOKEN)
+
+            'Using response As System.Net.Http.HttpResponseMessage = client.GetAsync(url).Result
+            Using response As System.Net.Http.HttpResponseMessage = client.GetAsync(url_get).Result
+                '生のレスポンス全体を文字列で取得
+                Dim responseBody As String = response.Content.ReadAsStringAsync().Result
+
+                'レスポンスの文字列をJSONとして解析されたJObjectに変換。
+                Dim jsonObj As Object = JsonConvert.DeserializeObject(responseBody)
+                'Dim oResponse As Newtonsoft.Json.Linq.JObject = CType(Newtonsoft.Json.JsonConvert.DeserializeObject(responseBody), Newtonsoft.Json.Linq.JObject)
+
+                'エラーコード判定
+                'Dim jsonObjarray As Array = (Array)jsonObj 
+                Try
+                    Dim err_code As String = jsonObj("code")
+                    If err_code Is Nothing Then
+                        Exit Try    'エラーなし
+                    End If
+                    Dim err_message As String = jsonObj("message")
+
+                    MessageBox.Show("code:" & err_code & vbCrLf & "message:" & err_message,
+                             "エラーメッセージ",
+                             MessageBoxButtons.OK)
+                    ' ボタンイベント関数を抜ける
+                    Exit Sub
+                Catch ex As Exception
+                    Throw
+                End Try
+
+
+                'レスポンスにrecordが存在するか確認
+                For Each get_records As Object In jsonObj("records")
+                    id = jsonObj("records")(0)("$id")("value")
+                    Exit For '一回目でループ抜ける
+                Next
+
+                'Debug.WriteLine(id)
+
+                '各値を出力
+                'Debug.WriteLine($"呼び出し成功 = {jsonObj}")
+            End Using
+            If id = "0" Then
+                '0(存在しなかった)なら登録
+                debug_text = "登録"
+                'POST用JSON定義
+                'Dim post_data = "{ ""app"": " & APP_ID & ", ""record"": { ""Detail"": { ""value"": """ & TextScaleConfirm.Text & """ } }}"
+                Dim post_data = "{ ""app"": " & APP_ID & ", ""record"": { ""品目"": { ""value"": ""総重量"" }, ""重量"": { ""value"": """ & TextScaleConfirm.Text & """ } }}"
+                Dim requestContent As New Net.Http.StringContent(post_data, System.Text.Encoding.UTF8, "application/json")
+
+                Using response As Net.Http.HttpResponseMessage = client.PostAsync(KINTONE_UPD_URL, requestContent).Result
+                    Dim responseBody As String = response.Content.ReadAsStringAsync().Result
+                    'レスポンスの文字列をJSONとして解析されたJObjectに変換。
+                    Dim jsonObj As Object = JsonConvert.DeserializeObject(responseBody)
+                    'エラーコード判定
+                    'Dim jsonObjarray As Array = (Array)jsonObj 
+                    Try
+                        Dim err_code As String = jsonObj("code")
+                        If err_code Is Nothing Then
+                            Exit Try    'エラーなし
+                        End If
+                        Dim err_message As String = jsonObj("message")
+
+                        MessageBox.Show("code:" & err_code & vbCrLf & "message:" & err_message,
+                             "エラーメッセージ",
+                             MessageBoxButtons.OK)
+                        ' ボタンイベント関数を抜ける
+                        Exit Sub
+                    Catch ex As Exception
+                        Throw
+                    End Try
+                    Debug.WriteLine(responseBody)
+                End Using
+            Else
+                '更新
+                debug_text = "更新"
+                'PUT用JSON定義
+                'Dim put_data = "{ 'app': 3, " & " 'id':'" & id & "' , 'record': { 'remarks': { 'value': '00005' } }}"
+                'Dim put_data = "{ ""app"": 3, ""id"": 3 , ""record"": { ""remarks"": { ""value"": ""00005"" } }}"
+                'Dim put_data = "{ ""app"": " & APP_ID & ", ""id"": " & id & ", ""record"": { ""Detail"": { ""value"": """ & TextScaleConfirm.Text & """ } }}"
+                Dim put_data = "{ ""app"": " & APP_ID & ", ""id"": " & id & ", ""record"": { ""重量"": { ""value"": """ & TextScaleConfirm.Text & """ } }}"
+                Dim requestContent As New Net.Http.StringContent(put_data, System.Text.Encoding.UTF8, "application/json")
+
+                Using response As Net.Http.HttpResponseMessage = client.PutAsync(KINTONE_UPD_URL, requestContent).Result
+                    Dim responseBody As String = response.Content.ReadAsStringAsync().Result
+                    'レスポンスの文字列をJSONとして解析されたJObjectに変換。
+                    Dim jsonObj As Object = JsonConvert.DeserializeObject(responseBody)
+                    'エラーコード判定
+                    Try
+                        Dim err_code As String = jsonObj("code")
+                        If err_code Is Nothing Then
+                            Exit Try    'エラーなし
+                        End If
+                        Dim err_message As String = jsonObj("message")
+
+                        MessageBox.Show("code:" & err_code & vbCrLf & "message:" & err_message,
+                             "エラーメッセージ",
+                             MessageBoxButtons.OK)
+                        ' ボタンイベント関数を抜ける
+                        Exit Sub
+                    Catch ex As Exception
+                        Throw
+                    End Try
+                    Debug.WriteLine(responseBody)
+                End Using
+            End If
+
+        End Using
+
+    End Sub
+
+
+End Class
+
+
+'INIファイルのアクセスクラス
+Public Class ClsIni
+    'プロファイル文字列取得.
+    'Private Declare Function GetPrivateProfileString Lib "kernel32" Alias "GetPrivateProfileStringA" ( _
+    '   ByVal lpApplicationName As String, _
+    '   ByVal lpKeyName As String, _
+    '   ByVal lpDefault As String, _
+    '   ByVal lpReturnedString As System.Text.StringBuilder, _[
+    '   ByVal nSize As UInt32, _
+    '   ByVal lpFileName As String) As UInt32
+    '宣言修正
+    Private Declare Function GetPrivateProfileString Lib "kernel32" Alias "GetPrivateProfileStringA" (
+        <MarshalAs(UnmanagedType.LPStr)> ByVal lpApplicationName As String,
+        <MarshalAs(UnmanagedType.LPStr)> ByVal lpKeyName As String,
+        <MarshalAs(UnmanagedType.LPStr)> ByVal lpDefault As String,
+        <MarshalAs(UnmanagedType.LPStr)> ByVal lpReturnedString As StringBuilder,
+        ByVal nSize As UInt32,
+        <MarshalAs(UnmanagedType.LPStr)> ByVal lpFileName As String) As UInt32
+
+    'プロファイル文字列書込み
+    'Private Declare Function WritePrivateProfileString Lib "kernel32" Alias "WritePrivateProfileStringA" ( _
+    '    ByVal lpAppName As String, _
+    '    ByVal lpKeyName As String, _
+    '    ByVal lpString As String, _
+    '    ByVal lpFileName As String) As Integer
+    '宣言修正
+    Private Declare Function WritePrivateProfileString Lib "kernel32" Alias "WritePrivateProfileStringA" (
+        <MarshalAs(UnmanagedType.LPStr)> ByVal lpAppName As String,
+        <MarshalAs(UnmanagedType.LPStr)> ByVal lpKeyName As String,
+        <MarshalAs(UnmanagedType.LPStr)> ByVal lpString As String,
+        <MarshalAs(UnmanagedType.LPStr)> ByVal lpFileName As String) As Integer
+
+    Private strIniFileName As String = ""
+
+    ''' <summary>
+    ''' コンストラクタ
+    ''' </summary>
+    ''' <param name="strIniFile">INIファイル名(フルパス)</param>
+    Sub New(ByVal strIniFile As String)
+        Me.strIniFileName = strIniFile  'ファイル名退避
+    End Sub
+
+    ''' <summary>
+    ''' プロファイル文字列取得
+    ''' </summary>
+    ''' <param name="strAppName">アプリケーション文字列</param>
+    ''' <param name="strKeyName">キー文字列</param>
+    ''' <param name="strDefault">デフォルト文字列</param>
+    ''' <returns>プロファイル文字列</returns>
+    Public Function GetProfileString(ByVal strAppName As String,
+                                     ByVal strKeyName As String,
+                                     ByVal strDefault As String) As String
+        Try
+            Dim strWork As System.Text.StringBuilder = New System.Text.StringBuilder(1024)
+            Dim intRet As Integer = GetPrivateProfileString(strAppName, strKeyName,
+                                                                       strDefault, strWork,
+                                                                       strWork.Capacity - 1, strIniFileName)
+            If intRet > 0 Then
+                'エスケープ文字を解除して返す
+                Return ResetEscape(strWork.ToString())
+            Else
+                Return strDefault
+            End If
+        Catch ex As Exception
+            Return strDefault
+        End Try
+    End Function
+
+    ''' <summary>
+    ''' プロファイル文字列設定
+    ''' </summary>
+    ''' <param name="strAppName">アプリケーション文字列</param>
+    ''' <param name="strKeyName">キー文字列</param>
+    ''' <param name="strSet">設定文字列</param>
+    ''' <returns>True:正常, False:エラー</returns>
+    Public Function WriteProfileString(ByVal strAppName As String,
+                                       ByVal strKeyName As String,
+                                       ByVal strSet As String) As Boolean
+        Try
+            'エスケープ文字変換
+            Dim strCnv As String = SetEscape(strSet)
+            Dim intRet As Integer = WritePrivateProfileString(strAppName, strKeyName, strCnv, strIniFileName)
+            If intRet > 0 Then
+                Return True
+            Else
+                Return False
+            End If
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
+
+    ''' <summary>
+    ''' エスケープ文字変換
+    ''' </summary>
+    ''' <param name="strSet">設定文字列</param>
+    ''' <returns>変換後文字列</returns>
+    Private Function SetEscape(ByVal strSet As String) As String
+        Dim strEscape As String = ";#=:"
+        Dim strRet As String = strSet
+        Try
+            For i = 0 To strEscape.Length - 1
+                Dim str As String = strEscape.Substring(i, 1)
+                strRet = strRet.Replace(str, "\" & str)
+            Next
+            Return strRet
+        Catch ex As Exception
+            Return ""
+        End Try
+    End Function
+
+    ''' <summary>
+    ''' エスケープ文字解除
+    ''' </summary>
+    ''' <param name="strSet">設定文字列</param>
+    ''' <returns>変換後文字列</returns>
+    Private Function ResetEscape(ByVal strSet As String) As String
+        Dim strEscape As String = ";#=:"
+        Dim strRet As String = strSet
+        Try
+            For i = 0 To strEscape.Length - 1
+                Dim str As String = strEscape.Substring(i, 1)
+                strRet = strRet.Replace("\" & str, str)
+            Next
+            Return strRet
+        Catch ex As Exception
+            Return ""
+        End Try
+    End Function
+End Class
+
+
